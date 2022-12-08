0014349c48b84fe001d6e2ad09fab29188d01b12
Willian Lima <willian.slima@mercadolivre.com>
2022-12-08 08:27:00 -0300

    exercicios

---
+++ 01_solid_srp_v2/src/solid_srp_v2/TestaLivro.java
@@ -0,0 +1,21 @@
+
+package solid_srp_v2;
+
+
+public class TestaLivro {
+
+	public static void main(String[] args) {
+	
+		
+		Livro livro = new Livro ("Livro 1","Autor1",1990, 100, "BR9999888777");
+		
+		Fatura fatura = new Fatura (livro, 10, 0.50, 0.50);
+		ImpressaoDeFatura print = new ImpressaoDeFatura(fatura);
+
+		print.imprimir();
+
+		
+	
+	}
+
+}
